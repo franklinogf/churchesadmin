374378bd936e0948e7c541444ebcf32f5d25c7b6
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-04-02 23:04:41 -0400

    Enhance AppServiceProvider with configuration methods for commands, dates, models, validations, and JSON resources

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 final class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +28,43 @@ final class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+        $this->configureDates();
+        $this->configureModels();
+        $this->configureValidations();
+        $this->configureJsonResources();
+    }
+
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(app()->isProduction());
+    }
+
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    private function configureModels(): void
+    {
+
+        Model::unguard();
+        Relation::enforceMorphMap([]);
+    }
+
+    private function configureValidations(): void
+    {
+        Password::defaults(fn () => app()->isProduction()
+        ? Password::min(8)->letters()
+            ->mixedCase()
+            ->numbers()
+            ->uncompromised()
+        : Password::min(6));
+
+    }
+
+    private function configureJsonResources(): void
+    {
+        JsonResource::withoutWrapping();
     }
 }
