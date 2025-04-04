c04991b7d5abe81cb5ce1c726b99699b08915bbc
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-04-04 18:08:10 -0400

    Implement Labeable interface in OfferingFrequency enum; add methods for options, labels, and frequency in days

--- app/Enums/OfferingFrequency.php
+++ app/Enums/OfferingFrequency.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace App\Enums;
 
+use App\Enums\Interfaces\Labeable;
 use App\Enums\Traits\EnumToArray;
 
-enum OfferingFrequency: string
+enum OfferingFrequency: string implements Labeable
 {
     use EnumToArray;
     case WEEKLY = 'weekly';
@@ -17,4 +18,61 @@ enum OfferingFrequency: string
     case SEMIANNUALLY = 'semiannually';
     case ANNUALLY = 'annually';
     case ONE_TIME = 'one_time';
+
+    /**
+     * Get the options for the enum.
+     *
+     * @return array
+     */
+    public static function options(): array
+    {
+        return [
+            self::WEEKLY->value => self::WEEKLY->label(),
+            self::BIWEEKLY->value => self::BIWEEKLY->label(),
+            self::MONTHLY->value => self::MONTHLY->label(),
+            self::BIMONTHLY->value => self::BIMONTHLY->label(),
+            self::QUARTERLY->value => self::QUARTERLY->label(),
+            self::SEMIANNUALLY->value => self::SEMIANNUALLY->label(),
+            self::ANNUALLY->value => self::ANNUALLY->label(),
+            self::ONE_TIME->value => self::ONE_TIME->label(),
+        ];
+    }
+
+    /**
+     * Get the label for the enum value.
+     *
+     * @return string
+     */
+    public function label(): string
+    {
+        return match ($this) {
+            self::WEEKLY => 'Every week',
+            self::BIWEEKLY => 'Every two weeks',
+            self::MONTHLY => 'Every month',
+            self::BIMONTHLY => 'Every two months',
+            self::QUARTERLY => 'Every three months',
+            self::SEMIANNUALLY => 'Every six months',
+            self::ANNUALLY => 'Every year',
+            self::ONE_TIME => 'One time only',
+        };
+    }
+
+    /**
+     * Get the frequency in days.
+     *
+     * @return int
+     */
+    public function frequencyInDays(): int
+    {
+        return match ($this) {
+            self::WEEKLY => 7,
+            self::BIWEEKLY => 14,
+            self::MONTHLY => 30,
+            self::BIMONTHLY => 60,
+            self::QUARTERLY => 90,
+            self::SEMIANNUALLY => 180,
+            self::ANNUALLY => 365,
+            self::ONE_TIME => 0,
+        };
+    }
 }
