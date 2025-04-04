002c0c6f9a14eeb58c873110435475e378bc5db4
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-04-03 23:53:45 -0400

    Update locale settings to use LanguageEnum values for improved consistency

--- config/app.php
+++ config/app.php
@@ -80,11 +80,11 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'en'),
+    'locale' => env('APP_LOCALE', App\Enums\LanguageEnum::ES->value),
 
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', App\Enums\LanguageEnum::EN->value),
 
-    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+    'faker_locale' => env('APP_FAKER_LOCALE', 'es_US'),
 
     /*
     |--------------------------------------------------------------------------
