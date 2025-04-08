028ed0ab0d58daf159506fde23c87778525b5e30
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-04-08 14:40:41 -0400

    feat: create CategorySeeder to seed regular tag into the database

--- database/seeders/TenantDatabaseSeeder.php
+++ database/seeders/TenantDatabaseSeeder.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
-use App\Enums\TagType;
 use App\Models\Address;
 use App\Models\Member;
 use App\Models\Missionary;
-use App\Models\Tag;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,6 +18,10 @@ final class TenantDatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            Tenants\CategorySeeder::class,
+        ]);
+
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
@@ -28,15 +30,6 @@ final class TenantDatabaseSeeder extends Seeder
         Member::factory(5)->has(Address::factory())->create();
 
         Missionary::factory(5)->has(Address::factory())->create();
-
-        Tag::create([
-            'name' => [
-                'en' => 'Regular',
-                'es' => 'Regular',
-            ],
-            'type' => TagType::CATEGORY->value,
-            'is_regular' => true,
-        ]);
 
     }
 }
