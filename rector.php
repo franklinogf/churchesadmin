38d8e05ee50a121db1f111119c654416d52f395d
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-04-10 16:06:55 -0400

    feat: add additional dead code rectors to configuration for improved code quality

--- rector.php
+++ rector.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
+use Rector\DeadCode\Rector\If_\RemoveAlwaysTrueIfConditionRector;
+use Rector\DeadCode\Rector\Stmt\RemoveUnreachableStatementRector;
 use Rector\EarlyReturn\Rector\Return_\ReturnBinaryOrToEarlyReturnRector;
 
 return RectorConfig::configure()
@@ -16,6 +18,13 @@ return RectorConfig::configure()
     ->withSkip([
         ReturnBinaryOrToEarlyReturnRector::class,
         RemoveUselessReturnTagRector::class,
+        RemoveUnreachableStatementRector::class => [
+            __DIR__.'/app/Policies',
+        ],
+        RemoveAlwaysTrueIfConditionRector::class => [
+            __DIR__.'/app/Policies',
+        ],
+
     ])
     ->withPreparedSets(
         deadCode: true,
