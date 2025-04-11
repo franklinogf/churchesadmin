f06663bfde2736d5ae08d086df0f59cc89823998
DarwinML2 <darwinmass.ml2@gmail.com>
2025-04-10 20:07:54 -0400

    added navbar

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,12 +1,29 @@
-import { Head } from '@inertiajs/react';
+import { Button } from '@/components/ui/button';
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from '@/components/ui/navigation-menu';
+import { NavBar } from '@/lib/utils';
+import { Link } from '@radix-ui/react-navigation-menu';
+import { TvIcon } from 'lucide-react';
 
 export default function Welcome() {
     return (
-        <div className="container flex h-screen flex-col items-center justify-center">
-            <Head title="Welcome" />
-            <a href="http://tenant1.churchroll.test" className="btn btn-primary">
-                Go to tenant
-            </a>
-        </div>
+        <main>
+            <header className="bg-background sticky top-0 z-40 h-16 w-full items-center justify-between px-4 py-2.5 shadow-xl">
+                <nav className="flex justify-between">
+                    <TvIcon />
+                    <NavigationMenu className="hidden md:flex">
+                        <NavigationMenuList>
+                            {NavBar.map(({ label, url }) => (
+                                <NavigationMenuItem key={label}>
+                                    <Link href={url}>
+                                        <NavigationMenuLink>{label}</NavigationMenuLink>
+                                    </Link>
+                                </NavigationMenuItem>
+                            ))}
+                            <Button>Start free trial</Button>
+                        </NavigationMenuList>
+                    </NavigationMenu>
+                </nav>
+            </header>
+        </main>
     );
 }
