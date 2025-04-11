51853aa38050284042dc8341636cfe6c67c2db96
DarwinML2 <darwinmass.ml2@gmail.com>
2025-04-10 20:09:18 -0400

    added const Navbar Links in utils

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+export const NavBar = [
+    {
+        label: 'Guide',
+        url: '/',
+    },
+    {
+        label: 'Pricing',
+        url: '/',
+    },
+    {
+        label: 'Log in',
+        url: '/',
+    },
+] as const;
