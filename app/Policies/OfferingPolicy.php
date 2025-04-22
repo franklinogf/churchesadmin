aee8d98dbb7dbd1c17795ca98d0b020ee33436b0
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-04-21 21:37:03 -0400

    feat: add offering and offering type permissions and update policies

--- app/Policies/OfferingPolicy.php
+++ app/Policies/OfferingPolicy.php
@@ -1,66 +1,60 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Policies;
 
-use App\Models\Offering;
+use App\Enums\TenantPermission;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class OfferingPolicy
+final class OfferingPolicy
 {
     /**
-     * Determine whether the user can view any models.
+     * Determine whether the user can view models.
      */
-    public function viewAny(User $user): bool
+    public function viewAny(User $user): Response
     {
-        return false;
-    }
+        if ($user->can(TenantPermission::MANAGE_OFFERINGS)) {
+            return Response::allow();
+        }
 
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Offering $offering): bool
-    {
-        return false;
+        return Response::deny(__('permission.view_any', ['label' => __('Offerings')]));
     }
 
     /**
      * Determine whether the user can create models.
      */
-    public function create(User $user): bool
+    public function create(User $user): Response
     {
-        return false;
+        if ($user->can(TenantPermission::CREATE_OFFERINGS)) {
+            return Response::allow();
+        }
+
+        return Response::deny(__('permission.create', ['label' => __('Offerings')]));
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Offering $offering): bool
+    public function update(User $user): Response
     {
-        return false;
+        if ($user->can(TenantPermission::UPDATE_OFFERINGS)) {
+            return Response::allow();
+        }
+
+        return Response::deny(__('permission.update', ['label' => __('Offerings')]));
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Offering $offering): bool
+    public function delete(User $user): Response
     {
-        return false;
-    }
+        if ($user->can(TenantPermission::DELETE_OFFERINGS)) {
+            return Response::allow();
+        }
 
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Offering $offering): bool
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Offering $offering): bool
-    {
-        return false;
+        return Response::deny(__('permission.delete', ['label' => __('Offerings')]));
     }
 }
