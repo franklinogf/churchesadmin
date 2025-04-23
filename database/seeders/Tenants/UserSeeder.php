f770d97ba81eb7d192494ce0effb0d6a3e26471d
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-04-23 18:12:06 -0400

    fix: assign SUPER_ADMIN role to the created user in UserSeeder

--- database/seeders/Tenants/UserSeeder.php
+++ database/seeders/Tenants/UserSeeder.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Database\Seeders\Tenants;
 
 use App\Enums\LanguageCode;
+use App\Enums\TenantRole;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -15,12 +16,13 @@ final class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
+        $user = User::create([
             'name' => 'Super Admin',
             'email' => 'superadmin@example.com',
             'email_verified_at' => now(),
             'password' => 'Password123',
             'language' => LanguageCode::EN->value,
         ]);
+        $user->assingRole(TenantRole::SUPER_ADMIN->value);
     }
 }
