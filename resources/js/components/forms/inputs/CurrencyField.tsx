84719bc498e8c13a849a26af10eadb2be0c050ca
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-05-01 13:10:38 -0400

    refactor: Simplify CurrencyField component by removing unused imports and restructuring props and not auto decimals

--- resources/js/components/forms/inputs/CurrencyField.tsx
+++ resources/js/components/forms/inputs/CurrencyField.tsx
@@ -3,15 +3,20 @@ import { FieldError } from '@/components/forms/inputs/FieldError';
 import { FieldLabel } from '@/components/forms/inputs/FieldLabel';
 import { Input } from '@/components/ui/input';
 import { cn } from '@/lib/utils';
-import { DollarSignIcon } from 'lucide-react';
 import { useId } from 'react';
-import CurrencyInput, { type CurrencyInputProps } from 'react-currency-input-field';
+import CurrencyInput from 'react-currency-input-field';
 
-interface CurrencyFieldProps extends Omit<CurrencyInputProps, 'onChange' | 'id'> {
+interface CurrencyFieldProps {
   onChange?: (value: string) => void;
   error?: string;
   label?: string;
   fieldClassName?: string;
+  required?: boolean;
+  disabled?: boolean;
+  className?: string;
+  value?: string;
+  placeholder?: string;
+  allowNegativeValue?: boolean;
 }
 
 export function CurrencyField({
@@ -24,40 +29,39 @@ export function CurrencyField({
   onChange,
   fieldClassName,
   placeholder = '0.00',
+  allowNegativeValue = false,
+
   ...props
 }: CurrencyFieldProps) {
   const id = useId();
   return (
     <FieldContainer className={className}>
       <FieldLabel disabled={disabled} id={id} label={label} required={required} />
-      <div className="relative w-full">
-        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-          <DollarSignIcon className="text-foreground/80 size-3.5" />
-        </div>
-        <CurrencyInput
-          id={id}
-          disabled={disabled}
-          required={required}
-          className={cn(
-            'ps-6',
-            {
-              'border-red-600 ring-offset-red-600 focus-visible:ring-red-600 dark:border-red-400 dark:ring-offset-red-400 dark:focus-visible:ring-red-400':
-                error,
-            },
-            fieldClassName,
-          )}
-          customInput={Input}
-          allowNegativeValue={false}
-          value={value}
-          fixedDecimalLength={2}
-          decimalScale={2}
-          onValueChange={(value) => {
-            onChange?.(value ?? '');
-          }}
-          placeholder={placeholder}
-          {...props}
-        />
-      </div>
+      <CurrencyInput
+        id={id}
+        disabled={disabled}
+        required={required}
+        className={cn(
+          {
+            'border-red-600 ring-offset-red-600 focus-visible:ring-red-600 dark:border-red-400 dark:ring-offset-red-400 dark:focus-visible:ring-red-400':
+              error,
+          },
+          fieldClassName,
+        )}
+        onFocus={(e) => {
+          e.target.select();
+        }}
+        customInput={Input}
+        allowNegativeValue={allowNegativeValue}
+        value={value}
+        prefix="$"
+        decimalScale={2}
+        onValueChange={(value) => {
+          onChange?.(value ?? '');
+        }}
+        placeholder={placeholder}
+        {...props}
+      />
 
       <FieldError error={error} />
     </FieldContainer>
