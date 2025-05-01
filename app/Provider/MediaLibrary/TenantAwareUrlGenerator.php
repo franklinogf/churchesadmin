1d769e8f16c45f3f99e6269f83cdd69d0baf0b15
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-05-01 15:50:18 -0400

    feat: implement TenantAwareUrlGenerator for custom URL generation in media library

---
+++ app/Provider/MediaLibrary/TenantAwareUrlGenerator.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Provider\MediaLibrary;
+
+use Spatie\MediaLibrary\Support\UrlGenerator\DefaultUrlGenerator;
+
+final class TenantAwareUrlGenerator extends DefaultUrlGenerator
+{
+    public function getUrl(): string
+    {
+        $pathRelative = $this->getPathRelativeToRoot();
+        $url = tenancy()->initialized ? tenant_asset($pathRelative) : asset($pathRelative);
+
+        $url = $this->versionUrl($url);
+
+        return $url;
+    }
+}
