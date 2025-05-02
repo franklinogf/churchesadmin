c976bb0ceb91cbc2b0d8e447084532a17e504d3a
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-05-02 19:04:21 -0400

    feat: create ExpenseTypeForm component and integrate into expense types page; update modal handling and remove old implementation

--- resources/js/pages/codes/expenseTypes/index.tsx
+++ resources/js/pages/codes/expenseTypes/index.tsx
@@ -1,22 +1,25 @@
 import { DataTable } from '@/components/custom-ui/datatable/data-table';
+import { ExpenseTypeForm } from '@/components/forms/expense-type-form';
 import { PageTitle } from '@/components/PageTitle';
 import { Button } from '@/components/ui/button';
 import AppLayout from '@/layouts/app-layout';
 import type { ExpenseType } from '@/types/models/expense-type';
 import { useLaravelReactI18n } from 'laravel-react-i18n';
-import { ExpenseTypeForm } from './components/ExpenseTypeForm';
+import { useState } from 'react';
 import { columns } from './includes/columns';
 
 export default function ExpenseTypesIndex({ expenseTypes }: { expenseTypes: ExpenseType[] }) {
   const { t } = useLaravelReactI18n();
+  const [open, setOpen] = useState(false);
   return (
     <AppLayout title={t('Expense Types')} breadcrumbs={[{ title: t('Expense Types') }]}>
       <PageTitle>{t('Expense Types')}</PageTitle>
+      <ExpenseTypeForm open={open} setOpen={setOpen} />
       <DataTable
         headerButton={
-          <ExpenseTypeForm>
-            <Button>{t('Add Expense Type')}</Button>
-          </ExpenseTypeForm>
+          <Button size="sm" onClick={() => setOpen(true)}>
+            {t('Add Expense Type')}
+          </Button>
         }
         data={expenseTypes}
         columns={columns}
