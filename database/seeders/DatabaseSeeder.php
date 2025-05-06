26753f7f212c81d4e768dd92a71d07cb0c8df7b8
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-05-06 12:58:16 -0400

    Fix locale assignment in DatabaseSeeder: use enum value for language code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,7 +28,7 @@ final class DatabaseSeeder extends Seeder
             Church::create([
                 'id' => 'test-church',
                 'name' => 'Test Church',
-                'locale' => LanguageCode::ENGLISH,
+                'locale' => LanguageCode::ENGLISH->value,
                 'active' => true,
             ])->createDomain('test');
         }
