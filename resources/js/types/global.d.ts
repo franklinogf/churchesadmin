fd6345b324131feb5b47737d1743389c3d821133
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-05-20 20:27:43 -0400

    refactor: extend ColumnMeta interface to include filterVariant and translationPrefix properties

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -3,3 +3,11 @@ import type { route as routeFn } from 'ziggy-js';
 declare global {
   const route: typeof routeFn;
 }
+
+declare module '@tanstack/react-table' {
+  //allows us to define custom properties for our columns
+  interface ColumnMeta {
+    filterVariant?: 'text' | 'range' | 'select';
+    translationPrefix?: string;
+  }
+}
