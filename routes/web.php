a817b507f736ef137a59b72d88edee96b401fc22
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-09-09 13:31:08 -0400

    fix: update church retrieval method to use name instead of ID for demo link

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ foreach (config('tenancy.identification.central_domains') as $domain) {
         Route::name('root.')
             ->group(function (): void {
                 Route::get('/', function (): Response {
-                    $church = Church::find('68c097c1f963');
+                    $church = Church::where('name', 'Demo Church')->first();
                     $url = create_tenant_url($church, 'login');
 
                     return Inertia::render('welcome', ['demoLink' => $url]);
